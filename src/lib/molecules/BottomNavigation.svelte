a9745ff180b0d026063813d3b0665bf33c1548a4
Daniel Mader <45594838+daniel-mader@users.noreply.github.com>
2023-03-10 13:02:55 +0100

    feat: add component: bottom navigation

---
+++ src/lib/molecules/BottomNavigation.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import { Clock, Cog, User } from 'svelte-heros-v2';
+
+  const dispatch = createEventDispatcher();
+
+  export let active: 'history' | 'profile' | 'settings' = 'profile';
+</script>
+
+<div class="flex justify-evenly sm:justify-center sm:space-x-24 py-2 border-t">
+  <button class="hover:bg-slate-100 rounded-full p-4" on:click={() => dispatch('history')}>
+    <Clock
+      class={`${active == 'history' ? 'text-violet-700' : 'text-slate-400'}`}
+      strokeWidth="2"
+    />
+  </button>
+  <button class="hover:bg-slate-100 rounded-full p-4" on:click={() => dispatch('profile')}>
+    <User class={`${active == 'profile' ? 'text-violet-700' : 'text-slate-400'}`} strokeWidth="2" />
+  </button>
+  <button class="hover:bg-slate-100 rounded-full p-4" on:click={() => dispatch('settings')}>
+    <Cog class={`${active == 'settings' ? 'text-violet-700' : 'text-slate-400'}`} strokeWidth="2" />
+  </button>
+</div>
---
+++ src/stories/molecules/BottomNavigation.stories.ts
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/svelte';
+
+import BottomNavigation from '$lib/molecules/BottomNavigation.svelte';
+
+// More on how to set up stories at: https://storybook.js.org/docs/7.0/svelte/writing-stories/introduction
+const meta = {
+  title: 'Molecules/Bottom Navigation',
+  component: BottomNavigation,
+  tags: ['autodocs'],
+  argTypes: {
+    active: {
+      control: { type: 'select' },
+      options: ['history', 'profile', 'settings']
+    }
+  }
+} satisfies Meta<BottomNavigation>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+// More on writing stories with args: https://storybook.js.org/docs/7.0/svelte/writing-stories/args
+export const Default: Story = {
+  args: {
+    // title: 'Graduate diploma',
+    // description: 'University of Oxford'
+  }
+};
