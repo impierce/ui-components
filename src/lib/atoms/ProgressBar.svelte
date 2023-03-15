9a244d6775a8956914e2ec80b24a6936ce84bd9d
Daniel Mader <45594838+daniel-mader@users.noreply.github.com>
2023-03-15 15:45:39 +0100

    feat: add ProgressBar component

---
+++ src/lib/atoms/ProgressBar.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  export let value: number;
+</script>
+
+<div class="relative pt-1">
+  <div class="overflow-hidden h-2 mb-4 text-xs flex rounded bg-violet-100">
+    <div
+      style="width:{value}%"
+      class="shadow-none flex flex-col text-center whitespace-nowrap text-white justify-center bg-gradient-to-r from-violet-600 to-pink-500 rounded-full"
+    />
+  </div>
+</div>
