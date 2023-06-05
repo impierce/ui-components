24eca3c41650a8fcaba356c5021269ac8d6d402c
Daniel Mader <daniel.mader@impierce.com>
2023-06-05 09:13:11 +0200

    feat: init shadcn-svelte

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,12 @@ const config = {
   // Consult https://kit.svelte.dev/docs/integrations#preprocessors
   // for more information about preprocessors
   preprocess: vitePreprocess(),
-
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    alias: {
+      $components: 'src/lib/components',
+      '$components/*': 'src/lib/components/*'
+    }
   }
 };
 
